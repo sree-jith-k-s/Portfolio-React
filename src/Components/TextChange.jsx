021f4f04025d4e2f974717d78e41efe78f6293e7
sree-jith-k-s <sreejithksreenivasan376@gmail.com>
2024-08-30 23:38:14 +0530

    30.08

--- src/Components/TextChange.jsx
+++ src/Components/TextChange.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+
 const TextChange = () => {
-  const texts = ["Hi, I'm Sreejith", "Hi, I'm Sreejith", "Hi, I'm Sreejith"];
-  const [currenText, setCurrentText] = useState("");
-  const [endValue, setendValue] = useState(true);
+  const texts = useMemo(() => ["Hi, I'm Sreejith", "Hi, I'm Sreejith", "Hi, I'm Sreejith"], []);
+  const [currentText, setCurrentText] = useState("");
+  const [endValue, setEndValue] = useState(1);
   const [isForward, setIsForward] = useState(true);
   const [index, setIndex] = useState(0);
 
@@ -10,23 +11,23 @@ const TextChange = () => {
     const intervalId = setInterval(() => {
       setCurrentText(texts[index].substring(0, endValue));
       if (isForward) {
-        setendValue((prev) => prev + 1);
+        setEndValue((prev) => prev + 1);
       } else {
-        setendValue((prev) => prev - 1);
+        setEndValue((prev) => prev - 1);
       }
       if (endValue > texts[index].length + 10) {
         setIsForward(false);
       }
-      if (endValue < 2.1) {
+      if (endValue < 2) {
         setIsForward(true);
-        setIndex((prev) => prev & texts.length);
+        setIndex((prev) => (prev + 1) % texts.length);
       }
     }, 50);
 
     return () => clearInterval(intervalId);
   }, [endValue, isForward, index, texts]);
 
-  return <div className="transition ease duration-300">{currenText}</div>;
+  return <div className="transition ease duration-300">{currentText}</div>;
 };
 
 export default TextChange;
